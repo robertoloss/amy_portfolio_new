90f2697ed25fe2f693def3fc7bab81d0cb957bd8
Roberto Loss <robertoloss@gmail.com>
2023-12-10 15:24:10 -0500

    removed actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,7 @@
-'use server'
- 
-import { revalidateTag } from 'next/cache'
- 
-export default async function action() {
-  revalidateTag('collection')
-}
+//'use server'
+// 
+//import { revalidateTag } from 'next/cache'
+// 
+//export default async function action() {
+//  revalidateTag('collection')
+//}
